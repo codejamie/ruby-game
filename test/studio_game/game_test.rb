1453bb766f6c946cc37c4a572d6b1fd4b561280a
codejamie <jamie.finlay@wearecodenation.com>
2024-12-12 19:39:57 +0000

    add game and player empty test files

---
+++ test/studio_game/game_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+
+require_relative "../../lib/studio_game/game"
+
+class GameTest < Minitest::Test
+
+end
---
+++ test/studio_game/player_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+
+require_relative "../../lib/studio_game/player"
+
+class PlayerTest < Minitest::Test
+
+end
